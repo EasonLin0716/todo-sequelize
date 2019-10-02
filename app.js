57225fd8ccee88bfc4575bd44eea9aa268a9ae8e
Eason Lin <uu9924079@gmail.com>
2019-10-02 18:59:04 +0800

    add routes and views for register and login pages

--- app.js
+++ app.js
@@ -18,8 +18,29 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 /* ----- Set routes ----- */
+// home
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+// login page
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// login check
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// register page
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// register check
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+// logout
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 /* ----- listen to port 3000 ----- */
