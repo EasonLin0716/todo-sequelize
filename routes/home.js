2ea94094b5690190224344f474903bdc6aadae68
Eason Lin <uu9924079@gmail.com>
2019-10-04 12:09:53 +0800

    add sort

--- routes/home.js
+++ routes/home.js
@@ -6,13 +6,19 @@ const User = db.User
 const { authenticated } = require('../config/auth')
 // list all Todo
 router.get('/', authenticated, (req, res) => {
+  let orderResult = []
+  if (req.query.sortTarget && req.query.sortType) {
+    orderResult.push([req.query.sortTarget, req.query.sortType])
+  }
+  console.log(orderResult)
   // get current user
   User.findByPk(req.user.id)
     .then((user) => {
       if (!user) throw new Error("user not found")
       // get current user's todo list
       return Todo.findAll({
-        where: { UserId: req.user.id }
+        where: { UserId: req.user.id },
+        order: orderResult
       })
     })
     .then((todos) => { return res.render('index', { todos: todos }) })
